97889ebfea6e9bd67fe434e2714e566f03942ea1
Eric Hong <ericdhong@gmail.com>
2017-04-15 00:59:32 -0700

    set title of newsfeed tab to be Feed

--- NPSmiles/Views/NewsFeedView.swift
+++ NPSmiles/Views/NewsFeedView.swift
@@ -10,26 +10,11 @@ import UIKit
 
 class NewsFeedView: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
+    self.title = "Feed"
+  }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
