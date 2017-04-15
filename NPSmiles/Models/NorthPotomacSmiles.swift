9aa5c2c6668feb00b717a51963f5d340255dd91b
Eric Hong <ericdhong@gmail.com>
2017-04-15 10:43:34 -0700

    Created a model with basic info including email, phone, fax, and legal name

---
+++ NPSmiles/Models/NorthPotomacSmiles.swift
@@ -0,0 +1,18 @@
+//
+//  NorthPotomacSmiles.swift
+//  NPSmiles
+//
+//  Created by Eric Hong on 4/15/17.
+//  Copyright © 2017 EricDHong. All rights reserved.
+//
+
+import Foundation
+
+struct NorthPotomacSmiles {
+  let legalName = "North Potomac Smiles, LLC."
+  let email = "info@northpotomacsmiles.com"
+  let phoneNumber = "301.926.4408"
+  let faxNumber = "301.926.4405"
+  let address = "15200 Shady Grove Rd, #408"
+  let cityState = "Rockville, MD 20850"
+}
