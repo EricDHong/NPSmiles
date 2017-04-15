0e96ab84508a42ea425f8cb263da9efcc95f72b6
Eric Hong <ericdhong@gmail.com>
2017-04-15 13:09:39 -0700

    Been reformatting this attributed text since around 930am with a lunch break but learning how to write cleaner code!!

--- NPSmiles/Controllers/ContactViewController.swift
+++ NPSmiles/Controllers/ContactViewController.swift
@@ -9,5 +9,41 @@
 import UIKit
 
 class ContactViewController: ContactView {
+  
+  let company = NorthPotomacSmiles()
+  let finalAttributedText = NSMutableAttributedString()
+  var finalTextArray = [String]()
+  
+  override func loadView() {
+    super.loadView()
+    setTextView()
+  }
+  
+  func setTextView() {
+    let fontAttribute = UIFont.systemFont(ofSize: 24.0, weight: UIFontWeightLight)
+    let nonlinkAttributes:[String:Any] = [ NSForegroundColorAttributeName : UIColor.black,
+                                           NSFontAttributeName : fontAttribute]
+    let linkAttributes:[String:Any] = [ NSForegroundColorAttributeName : UIColor.blue,
+                                        NSFontAttributeName : fontAttribute]
+    
+    let emailText = "\(company.email)\n"
+    let phoneText1 = "Telephone: "
+    let phoneText2 = "\(company.phoneNumber)\n"
+    let faxText1 = "Fax: "
+    let faxText2 = "\(company.phoneNumber)\n"
+    
+    finalTextArray = [emailText, phoneText1, phoneText2, faxText1, faxText2]
+    for index in 0..<finalTextArray.count {
+      attributeText(of: finalTextArray[index], with: (index % 2 == 0) ? linkAttributes : nonlinkAttributes)
+    }
+    contactInfoTextView.attributedText = finalAttributedText
+  }
+  
+  func attributeText(of text:String,with attributes:[String:Any]) {
+    let text = text
+    let attrText = NSMutableAttributedString(string: text)
+    attrText.addAttributes(attributes, range: NSRange(0..<text.characters.count))
+    finalAttributedText.append(attrText)
+  }
 
 }
