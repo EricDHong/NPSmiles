6c1a5000925df873066949bc9b6b257396301075
Eric Hong <ericdhong@gmail.com>
2017-04-15 01:04:15 -0700

    Created ContactView and ContactViewController where ContactViewController inherits from ContactView

---
+++ NPSmiles/Controllers/ContactViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ContactViewController.swift
+//  NPSmiles
+//
+//  Created by Eric Hong on 4/15/17.
+//  Copyright © 2017 EricDHong. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: ContactView {
+
+}
