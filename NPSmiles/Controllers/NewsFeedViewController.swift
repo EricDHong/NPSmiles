2c482c8fd10905da83f727951b19718df4b3e88c
Eric Hong <ericdhong@gmail.com>
2017-04-17 14:08:42 -0700

    added newsFeed:UICollectionView to NewsFeedView and NewsFeedViewController

--- NPSmiles/Controllers/NewsFeedViewController.swift
+++ NPSmiles/Controllers/NewsFeedViewController.swift
@@ -8,28 +8,27 @@
 
 import UIKit
 
-class NewsFeedViewController: NewsFeedView {
+class NewsFeedViewController: NewsFeedView, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UICollectionViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    newsFeed?.dataSource = self
+    newsFeed?.delegate = self
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 10
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellId", for: indexPath)
+    cell.backgroundColor = .yellow
+    return cell
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    let newsFeedSize = newsFeed?.bounds.size
+    return CGSize(width: newsFeedSize!.width, height: 150)
+  }
 
 }
--- NPSmiles/Views/NewsFeedView.swift
+++ NPSmiles/Views/NewsFeedView.swift
@@ -9,16 +9,29 @@
 import UIKit
 
 class NewsFeedView: UIViewController {
+  
+  private let flowLayout = UICollectionViewFlowLayout()
+  internal var newsFeed:UICollectionView?
 
+  override func loadView() {
+    super.loadView()
+    setupNewsFeed()
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     self.navigationItem.title = "Feed"
-  }
-
-  func setupViews() {
     
   }
   
-  
-
+  private func setupNewsFeed() {
+    newsFeed = UICollectionView(frame: view.bounds, collectionViewLayout: flowLayout)
+    flowLayout.minimumLineSpacing = 1.0
+    newsFeed?.backgroundColor = .red
+    newsFeed?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellId")
+    view.addSubview(newsFeed!)
+    newsFeed?.translatesAutoresizingMaskIntoConstraints = false
+    configureConstraint(with: "H:|[v0]|", to: view, of: ["v0":newsFeed!])
+    configureConstraint(with: "V:|[v0]|", to: view, of: ["v0":newsFeed!])
+  }
 }
