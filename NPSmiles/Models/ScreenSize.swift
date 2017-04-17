ae264dea3eb00b0288bc3b5fcc33f872eea42985
Eric Hong <ericdhong@gmail.com>
2017-04-16 19:55:48 -0700

    Added struct ScreenSize so wouldnt have to keep creating local var equal to self.view.bounds.size

---
+++ NPSmiles/Models/ScreenSize.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenSize.swift
+//  NPSmiles
+//
+//  Created by Eric Hong on 4/16/17.
+//  Copyright © 2017 EricDHong. All rights reserved.
+//
+
+import UIKit
+
+struct ScreenSize {
+  static let height = UIScreen.main.bounds.height
+  static let width = UIScreen.main.bounds.width
+}
