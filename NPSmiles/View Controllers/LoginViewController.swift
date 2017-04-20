f6526b422f82f8d4dd9567ad4d8706c9243108d0
Eric Hong <ericdhong@gmail.com>
2017-04-20 13:54:13 -0700

    Created a LoginViewController and inherit from LoginView

---
+++ NPSmiles/View Controllers/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  NPSmiles
+//
+//  Created by Eric Hong on 4/20/17.
+//  Copyright © 2017 EricDHong. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: LoginView {
+
+}
---
+++ NPSmiles/Views/LoginView.swift
@@ -0,0 +1,30 @@
+//
+//  LoginView.swift
+//  NPSmiles
+//
+//  Created by Eric Hong on 4/20/17.
+//  Copyright © 2017 EricDHong. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIViewController {
+  let titleLabel: UILabel = {
+    let label = UILabel()
+    
+    return label
+  }()
+  
+  let adminLoginButton: UIButton = {
+    let button = UIButton()
+    
+    return button
+  }()
+  
+  let guestLoginButton: UIButton = {
+    let button = UIButton()
+    
+    return button
+  }()
+
+}
